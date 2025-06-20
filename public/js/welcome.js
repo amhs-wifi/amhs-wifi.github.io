84fa613228e92a33fc22efc8c87ed5720b85a9c9
meta-cognition <76786173+meta-cognition@users.noreply.github.com>
2025-06-19 20:47:34 -0800

    v2

---
+++ public/js/welcome.js
@@ -0,0 +1,119 @@
+// 2024.12.07 - modified by Dom Pannone for Wi-Fi pilot Project on Flagship Columbia!
+
+// Parse paramaters
+var base_grant_url = decodeURIComponent(GetURLParameter("base_grant_url"));
+// Make.com webhook:
+var user_continue_url = "https://hook.us2.make.com/js4kmicrcfvuzvwpjkmtmjqkhwfodklj";
+
+var user_continue_url_original = GetURLParameter("user_continue_url");
+var node_mac = GetURLParameter("node_mac");
+var client_ip = GetURLParameter("client_ip");
+var client_mac = GetURLParameter("client_mac");
+var vessel = GetURLParameter("vessel");
+var data = {};
+const cookieName = "wifiUniqueDeviceID";
+
+checkAndRedirect();
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    document.getElementById('loginForm').onsubmit= function(e){
+        e.preventDefault(); //prevents default form submission process to allow login and validation
+        login();
+    }
+    
+});
+
+// Button handler function to store the form data and login. 
+function login(){
+    // send the data somewhere like a database
+
+    data.email = document.getElementById("email").value;
+    if (validateEmail(data.email))
+    {
+        // Complete Login
+        authUser();
+    }
+}
+
+// if email is valid
+function authUser(){
+    var loginUrl = base_grant_url;
+    
+    user_continue_url += "?vessel="+vessel+"&r=0"+"&email="+btoa(data.email)+"&cip="+client_ip+"&cmac="+client_mac+"&apmac="+node_mac;
+    user_continue_url = encodeURIComponent(user_continue_url);
+    
+    loginUrl += "?continue_url="+user_continue_url;
+
+    //const uniqueID = generateUniqueID();
+    const uniqueID = btoa(data.email);
+    setCookie(cookieName, uniqueID, 36);
+
+    window.location.href = loginUrl;
+}
+
+// Helper function to parse URL
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+function validateEmail() {
+    const emailField = document.getElementById('email');
+    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+    if (!emailPattern.test(emailField.value)) {
+        alert('Please enter a valid email address.');
+        return false; // Prevent form submission
+    }
+    return true; // Allow form submission
+}
+
+function setCookie(name, value, hours) {
+    const date = new Date();
+    date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/; Secure; SameSite=Strict`;
+}
+
+function getCookie(name) {
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+function generateUniqueID() {
+    return 'id-' + Math.random().toString(36).substring(2, 16) + '-' + Date.now();
+}
+
+function checkAndRedirect() {
+    const redirectURL = user_continue_url_original;
+    const cookieValue = getCookie(cookieName);
+
+    if (cookieValue) {
+        // Cookie exists, redirect to another page
+        var loginUrl = base_grant_url;
+       
+        user_continue_url += "?vessel="+vessel+"&r=1"+"&email="+cookieValue+"&cip="+client_ip+"&cmac="+client_mac+"&apmac="+node_mac;
+        user_continue_url = encodeURIComponent(user_continue_url);
+        
+        loginUrl += "?continue_url="+user_continue_url;
+        document.write('<!--');
+        window.location.href = loginUrl;
+    } 
+}
---
+++ v2 Captive Portal/Relay/crew/index.php
@@ -0,0 +1,61 @@
+<?php
+
+$url = "https://ferrywifi.com/crew/?" ;
+$url .= "vessel=" . $_GET['vessl'] ;
+$url .= "&node_mac=" . $_GET['ap_mac'] ;
+$url .= "&client_mac=" . $_GET['client_mac'] ;
+$url .= "&client_ip=" . $_GET['client_ip'] ;
+$url .= "&base_grant_url=" . rawurlencode($_GET['base_grant_url']) ;
+
+?>
+<!--
+This page contains software contributions from:
+----
+Cory Guynn
+www.InternetOfLEGO.com
+2016
+----
+Dom Pannone
+AMHS/DOT&PF 
+2024
+----
+LICENSE: MIT
+-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+        <script src="./js/meraki.js"></script>
+        <style>
+            .custom-btn {
+                display: inline-block;
+                background-color: #343434; /* Adjust to match the original button's background color */
+                color: #ffffff; /* Adjust to match the original button's text color */
+                padding: 10px;
+                text-align: center;
+                text-decoration: none;
+                font-size:15px;
+                width: 100%;
+                line-height:16px;
+                transition: background-color 0.3s ease;
+                border: none;
+            }
+            .custom-btn:hover {
+                background-color: #ac5353; /* Adjust to match the original button's hover color */
+                text-decoration: none;
+            }
+        </style>
+    </head>
+    <div class="container">
+        
+      <form id="loginForm" action="#">
+        <center>Alaska Department of Transportation & Public Facilities</center>
+            <center>
+                <img src="./amhs-logo-crew.png" alt="Alaska Marine Highway Logo" style="width:256px; margin-top:15px; height:auto;">
+            </center>
+        <br>
+        <a href="<? echo $url;?>" target="_blank" rel="noopener noreferrer" class="custom-btn" onclick="window.open(this.href, '_blank'); return false;">Connect to Crew Priority Wi-Fi</a>
+      </form>
+    </div>
+</html>
+ 
